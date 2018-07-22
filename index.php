30e259f9b86bbb7d247f937ee09b461bdac8fab3
Mariana Victoria <mari.vic.resende@gmail.com>
2018-07-22 17:49:31 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>  
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pede Meia</title>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="pedeMeia"><h1>Pede Meia</h1></div>
+            <a class="navbar-brand logIn" href="#">Log in</a>
+        </div>        
+        
+        <div class="row">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Inicio</a>
+                    <a class="navbar-brand" href="#">Produtos</a>
+                    <a class="navbar-brand" href="#">Promoções</a>
+                    <a class="navbar-brand" href="#">Quem somos</a>
+                    <a class="navbar-brand" href="#">Contato</a>
+                </div>            
+            </nav>
+        </div>
+
+        <div class="row espaco">
+            
+        </div>
+        
+        <div class="row">
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+
+        </div>
+
+        <div class="row espaco">
+            
+        </div>
+        
+        <div class="row">
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+            
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="polaroid"><img class="img" src="assets/img/teste.jpeg" alt="teste"></div>    
+            </div>
+
+        </div>
+
+        <div class="row espaco">
+            
+        </div>
+
+        <div class="row">
+            <footer>não sei o que vai aqui</footer>
+        </div>
+        
+    </div>    
+</body>
+</html>
